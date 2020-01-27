4084d37159c1889b4e66896f8cbe0aa1805b13b8
Diego Vasquez <botellongo69@outlook.com>
2020-01-27 18:38:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
   monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
   monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
   
-  monopoly[:names][]
+  monopoly[:railroads][:names][]
 end
 
 def monopoly_with_fourth_tier
