8fe86bfd307cbf8c158e9e9ef39da85cfc65a3d9
Diego Vasquez <botellongo69@outlook.com>
2020-01-27 18:35:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,10 @@ end
 
 def monopoly_with_second_tier
   monopoly = monopoly()
-  monopoly[:railroads][:pieces] = 4 
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:names] = {}
+  monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly
 end
 
 def monopoly_with_third_tier
